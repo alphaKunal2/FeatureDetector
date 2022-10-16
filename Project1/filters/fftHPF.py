2531b24ba4fcb9e18429c88773ac11d8ac167f2e
Kunal <narang123kunal@gmail.com>
2022-10-16 21:56:14 +0530

    Added HPF

---
+++ Project1/filters/fftHPF.py
@@ -0,0 +1,36 @@
+
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+import math
+
+
+def fftHPF(path):
+    img = cv2.imread(path,0)
+
+    dft = cv2.dft(np.float32(img),flags = cv2.DFT_COMPLEX_OUTPUT)
+    dft_shift = np.fft.fftshift(dft)
+
+    magnitude_spectrum = 20*np.log(cv2.magnitude(dft_shift[:,:,0],dft_shift[:,:,1]))
+
+
+
+    rows, cols = img.shape
+    crow,ccol = math.ceil(rows/2) , math.ceil(cols/2)
+
+    # create a mask first, center square is 1, remaining all zeros
+    mask = np.zeros((rows,cols,2),np.uint8)
+    mask[crow-30:crow+30, ccol-30:ccol+30] = 1
+
+    # apply mask and inverse DFT
+    fshift = dft_shift*mask
+    f_ishift = np.fft.ifftshift(fshift)
+    img_back = cv2.idft(f_ishift)
+    img_back = cv2.magnitude(img_back[:,:,0],img_back[:,:,1])
+
+
+
+    # cv2.normalize(img_back, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+
+    return img_back
+
