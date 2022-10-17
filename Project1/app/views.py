deba5d4d8c63bb5f833c8645d327f57796a78124
Kunal <narang123kunal@gmail.com>
2022-10-17 23:13:58 +0530

    Image is now getting transformed

--- Project1/app/views.py
+++ Project1/app/views.py
@@ -4,24 +4,46 @@ from django.shortcuts import render,redirect
 from filters import gabor
 from django.http import HttpResponse
 from django.views import View
+import base64
+from PIL import Image
+import cv2
+import io
+import numpy as np
 # Create your views here.
 
 def home(request):
     if(request.method == 'GET'):
         return render(request,'index.html')
+def stringToCv2(base64_string):
+    imgdata = base64.b64decode(base64_string)
+    data =  Image.open(io.BytesIO(imgdata))
+    return cv2.cvtColor(np.array(data), cv2.COLOR_BGR2RGB)
+
+# convert PIL Image to an RGB image( technically a numpy array ) that's compatible with opencv
+
+    
+
+def toString(img):
+    _, im_arr = cv2.imencode('.jpg', img)
+    im_bytes = im_arr.tobytes()
+    im_b64 = base64.b64encode(im_bytes)
+    
+    return im_b64
 
 def filter_view(request,num):
     nums = [1,2,3,4,5,6]
     if(num in nums):
         if(request.method == 'GET'):
-                return render(request,'filter.html')
+                return render(request,'filter.html',{"outputImg":'null'})
         if(request.method == 'POST'):
-            if(num==1):
+            if(True):
                 img = request.POST.get('img')
-
-                new_img = gabor.Gabor(img)
-                print(len(new_img))
-                return render(request,'filter.html',{'img':new_img})
+                
+                cv2img = stringToCv2(img)
+                print(cv2img.shape)
+                new_img = gabor.Gabor(cv2img)
+                print(toString(new_img))
+                return render(request,'filter.html',{"outputImg":toString(new_img)})
             # elif(num==2):
             # elif(num==3):
             # elif(num==4):
