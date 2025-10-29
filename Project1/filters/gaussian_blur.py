4d730ec19b6e669b525101ccff63497fce9ee4be
kunal118 <100676364+kunal118@users.noreply.github.com>
2025-10-29 22:27:38 +0530

    Create gaussian_blur.py

---
+++ Project1/filters/gaussian_blur.py
@@ -0,0 +1,18 @@
+from skimage.io import imread
+from skimage.color import rgb2gray
+from scipy.ndimage import gaussian_filter
+import matplotlib.pylab as plt
+
+def gaussian_blur(img, sigma=2):
+    """
+    Apply Gaussian blur in the spatial domain.
+    
+    Parameters:
+        img : ndarray
+            Input RGB or grayscale image.
+        sigma : float
+            Standard deviation of the Gaussian kernel (controls blur amount).
+    """
+    im_gray = rgb2gray(img)
+    blurred = gaussian_filter(im_gray, sigma=sigma)
+    return im_gray, blurred
